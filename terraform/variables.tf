b575c1a6c6e3323aa18f8dc0ae0c932e31a7c01c
Ludovic Logiou <ludovic.logiou@objectif-libre.com>
2020-09-13 14:36:56 +0200

    [Variables] Update variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,27 @@
-variable "project" {
-  type = string
-}
-
+// Gitlab variables
 variable "gitlab_token" {
   type = string
 }
 variable "gitlab_project_id" {
   type = number
+}
+
+// GCP variables
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "europe-west4"
+}
+
+variable "cidr_range" {
+  type = string
+  default = "192.168.0.0/24"
+}
+
+variable "vpc_connect_cidr_range" {
+  type = string
+  default = "10.8.0.0/28"
 }
