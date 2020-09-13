2ba0338f59e0b4fd8e753d49e2a5419fb3096a1c
Ludovic Logiou <ludovic.logiou@objectif-libre.com>
2020-09-13 15:24:23 +0200

    Fix services requirements

--- terraform/services.tf
+++ terraform/services.tf
@@ -1,9 +1,24 @@
-resource "google_project_service" "build" {
-  service            = "cloudbuild.googleapis.com"
+resource "google_project_service" "registry" {
+  service            = "containerregistry.googleapis.com"
   disable_on_destroy = false
 }
 
-resource "google_project_service" "registry" {
-  service            = "containerregistry.googleapis.com"
+resource "google_project_service" "network" {
+  service            = "servicenetworking.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "redis" {
+  service            = "redis.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "vpcaccess" {
+  service            = "vpcaccess.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "run" {
+  service            = "run.googleapis.com"
   disable_on_destroy = false
 }
