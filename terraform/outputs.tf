51cb02cf55e5d380be383ce5b01447e3803f62db
Ludovic Logiou <ludovic.logiou@objectif-libre.com>
2020-09-13 14:36:24 +0200

    [Outputs] Output Serverless connector uri

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "registry" {
   value = data.google_container_registry_repository.registry.repository_url
 }
+
+output "connection_uri" {
+  value = google_vpc_access_connector.connector.id
+}
